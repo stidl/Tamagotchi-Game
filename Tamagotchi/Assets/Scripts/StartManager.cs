f17fdd651e31161f4d68b98ad400ce8920ebb94a
stidl <m.stidl@gmx.net>
2022-02-17 18:46:06 +0100

    Add logic to pop up when click on load save but no save is found

--- Tamagotchi/Assets/Scripts/StartManager.cs
+++ Tamagotchi/Assets/Scripts/StartManager.cs
@@ -7,12 +7,20 @@ using UnityEngine.UI;
 public class StartManager : MonoBehaviour
 {
    
+public GameObject noSave;
+
     public void NewGame(){
         SceneManager.LoadScene("CharactorCreatorScene");
     }
 
     public void LoadGame(){
-        SceneManager.LoadScene("CharactorCreatorScene");
+
+        if(PlayerPrefs.GetFloat("hungerValue") == 0){
+        StartCoroutine(NoSaveGameText());
+        }else{
+        SceneManager.LoadScene("LoadSaveScene");
+        }
+        
     }
 
     public void QuitGame(){
@@ -23,8 +31,16 @@ public class StartManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         //disable no game saved display
+       noSave.gameObject.SetActive(false);
     }
+
+        IEnumerator NoSaveGameText(){
+        noSave.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        noSave.gameObject.SetActive(false);
+    }
+
 
     // Update is called once per frame
     void Update()
