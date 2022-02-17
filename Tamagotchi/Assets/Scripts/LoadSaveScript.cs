2f79dec3572815d714c2b293240cd70f6b4857ba
stidl <m.stidl@gmx.net>
2022-02-17 18:49:14 +0100

    Load data from save file and display it

---
+++ Tamagotchi/Assets/Scripts/LoadSaveScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSaveScript : MonoBehaviour
+{
+
+public Text nameText;
+public Text typeOfPetText;
+public Text hungerText;
+public Text hygieneText;
+public Text funText;
+public Text energyText;
+public Text numberOfClicksText;
+
+public static bool loadSave = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = PlayerPrefs.GetString("Name");
+        typeOfPetText.text = PlayerPrefs.GetInt("petType").ToString();
+        hungerText.text = PlayerPrefs.GetFloat("hungerValue").ToString();
+        hygieneText.text = PlayerPrefs.GetFloat("hygieneValue").ToString();
+        funText.text = PlayerPrefs.GetFloat("happinessValue").ToString();
+        energyText.text = PlayerPrefs.GetFloat("sleepValue").ToString();
+        numberOfClicksText.text = PlayerPrefs.GetInt("foodBtnClicks").ToString();
+    }
