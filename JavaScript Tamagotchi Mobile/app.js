d83675f14a2cff939eed5fcfc296e798d9ccc4d6
stidl <m.stidl@gmx.net>
2022-02-18 21:59:27 +0100

    Add variables for needs

--- JavaScript Tamagotchi Mobile/app.js
+++ JavaScript Tamagotchi Mobile/app.js
@@ -1,4 +1,12 @@
 
+var hunger = 100;
+var clock1 = setInterval(depleteHunger, 350); //Hunger decrement Speed
+var fun = 100;
+var clock2 = setInterval(depleteFun, 350);
+var hygiene = 100;
+var clock3 = setInterval(depleteHygiene, 350);
+var energy = 100;
+var clock4 = setInterval(depleteEnergy, 350);
 
   }
   }
